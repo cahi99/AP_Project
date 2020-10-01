f25e2814f398ce3e4dacdbe9abae4a139e8ee401
cahi-main <cahi-main@pop-os.localdomain>
2020-10-01 11:38:10 -0500

    estadoooo

--- codigo/Mi_Pedido.py
+++ codigo/Mi_Pedido.py
@@ -1,7 +1,32 @@
+from kivy.app import App
 from kivymd.app import MDApp
+from kivymd.uix.card import MDCard
+from kivymd.uix.list import TwoLineListItem
+from kivymd.uix.gridlayout import MDGridLayout
+from kivy.lang import Builder
+from kivymd.uix.menu import  RightContent
+
+class MyCard(MDCard):
+    pass
+class Mylist(TwoLineListItem):
+    def __init__(self, *args, **kwargs):
+        super(Mylist,self).__init__(**kwargs)
+        self.text=args[0]
+        self.secondary_text= 'Distribuidores'
+
+class Subtotales(RightContent):
+    def __init__(self, *args, **kwargs):
+        super(Subtotales,self).__init__(**kwargs)
+        self.ids.val.text= args[0]
+        self.ids.lbl.text= args[1]
 
 class MipedidoApp(MDApp):
     def build(self):
-        pass
+        pd=Builder.load_file('mipedido.kv')
+        for i in range(20):
+            pd.ids.scrll_v.add_widget(Mylist('producto '+str(i)))
+        for i,j in zip(['Valor Subtotal','IVa','Valor Total','Valor Total + Iva'],['88','7','82','89']):
+            pd.ids.conjuntos.add_widget(Subtotales(i,j))
+        return pd
 
 MipedidoApp().run()
--- codigo/Productos.py
+++ codigo/Productos.py
@@ -1,116 +1,28 @@
+from kivy.app import App
 from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.card import MDCard
-from kivymd.uix.label import MDLabel
-from kivymd.uix.imagelist import SmartTileWithLabel
-from kivy.properties import ObjectProperty
-from kivy.properties import StringProperty
+from kivy.lang import Builder
 from kivymd.uix.menu import  RightContent
 
-KV='''
-<RightContentCls>
-    MDIconButton:
-        icon:'plus'
-        pos_hint: {"center_x": .5, "center_y": .5}
-    
-    MDLabel:
-        text: "0"
-        theme_text_color: "Secondary"
-        hading: 'center'
-    MDIconButton:
-        icon:'minus'
-        pos_hint: {"center_x": .5, "center_y": .5}
-
-
-<MyCard@MDCard>
-    cards:cards
-    n_producto:n_producto
-    id:cards
-    orientation: "vertical"
-    padding: "10dp"
-    spading: "10dp"
-    size_hint: .5, None
-    size: "260dp", "180dp"
-    pos_hint: {"center_x": .5, "center_y": .5}
-    SmartTileWithLabel:
-        source: "Imagenes/Productos/plato_6.jpg"
-        width:self.height
-    MDGridLayout:
-        cols:2
-        adaptive_height: True
-        MDLabel:
-            id:n_producto
-            text: root.nombres
-            theme_text_color: "Secondary"
-            size_hint_y: None
-            height: self.texture_size[1]
-            pos_hint: {"center_x": .5, "center_y": .5}
-            hading: 'center'
-        RightContentCls:
-            hading: 'center'
-    
-Screen:
-    NavigationLayout:
-        ScreenManager:
-            Screen:
-                BoxLayout:
-                    orientation: "vertical"
-                    MDToolbar:
-                        title: "Productos"
-                        left_action_items:[['menu',lambda x: nav_drawer.toggle_nav_drawer()]]
-                        right_action_items:[['cart',lambda x: nav_drawer.toggle_nav_drawer()]]
-                        elevation:5
-                
-                    ScrollView:
-                        MDGridLayout:
-                            id:box
-                            cols: 3
-                            adaptive_height: True
-                            padding: dp(8), dp(8)
-                            spacing: dp(8), dp(8)
-        MDNavigationDrawer:
-            id: nav_drawer
-            BoxLayout:
-                orientation:'vertical'
-                spacing: '6dp'
-                padding: '6dp'
-                Image:
-                    source: 'Imagenes/2.jpg'
-                MDLabel:
-                    text: 'Mi Tienda'
-                    font_style: 'Subtitle1'
-                    size_hint_y: None
-                    height: self.texture_size[1]
-                ScrollView:
-                    MDList:
-                        OneLineIconListItem:
-                            text: 'Distribuidores'
-                            IconLeftWidget:
-                                icon: 'store'
-                        OneLineIconListItem:
-                            text: 'Mis Pedidos'
-                            IconLeftWidget:
-                                icon: 'cart'
-'''
 class MyCard(MDCard):
-    pass
-class RightContentCls(RightContent):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(MyCard,self).__init__(**kwargs)
+        self.ids.n_producto.text=args[0]
+        self.ids.id_image.source=args[1]
+        self.ids.plus.icon='plus'
+        self.ids.cero.text='0'
+        self.ids.minus.icon='minus'
+
+
 
 class ProductosApp(MDApp):
-    cards = ObjectProperty(None)
-    nombres= StringProperty('0')
-    n_producto = ObjectProperty(None)
     def build(self):
-        screen = Builder.load_string(KV)
-        for i in range(8):
-            screen.ids.box.add_widget(
-                MyCard()
-            )
-        for i in screen.ids:
-            print(self.n_producto.text)
-        return screen
+        DR =Builder.load_file('productos.kv')
+        for i,j in zip(['Plato 6','Plato 8','Plato 9'],['plato_6.jpg','plato_8.jpg','plato_9.jpg']):
+            DR.ids.box.add_widget(MyCard(i,"codigo/Imagenes/Productos/"+j))
+        #DR.ids.box.add_widget(MyCard("Plato 8","codigo/Imagenes/Productos/plato_8.jpg"))
+        #DR.ids.box.add_widget(MyCard("Plato 9","codigo/Imagenes/Productos/plato_9.jpg"))
+        return DR
 
-
-ProductosApp().run()
+if __name__ == '__main__':
+    ProductosApp().run()
